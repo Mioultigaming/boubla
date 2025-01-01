44642f80941274b0f09dd5ebf9f169af746fd6e9
Mioultigaming <52372852+Mioultigaming@users.noreply.github.com>
2025-01-01 02:57:21 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,9 +1,1 @@
-local function test()
-  for _, plr in pairs(game.Players:GetPlayers()) do
-    _G:xpxpxpssxpax(plr,"set",0)
-  end
-end
-
-while task.wait(5) do
-  test()
-end
+print("testtkIOJHDSGKNkk")
